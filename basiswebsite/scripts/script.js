1370137f445a59d9602f0094e8266696cdc4a1e7
sanotje <90205162+sanotje@users.noreply.github.com>
2021-09-17 11:38:36 +0200

    Add files via upload

---
+++ basiswebsite/scripts/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+console.log('hoi');
+
+var hamburgermenu = document.querySelector('.hamburgermenu');
+var navigatie = document.querySelector('.navigatie');
+
+hamburgermenu.addEventListener('click',menuUitklappen);
+
+function menuUitklappen(){
+navigatie.classList.toggle('uitklappen');
+}
